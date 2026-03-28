cfe38abf940f206da3e981703550460db5162d33
Elias <csoreghelias@gmail.com>
2026-03-28 14:22:23 +0100

    dragon food

---
+++ .minecraft/kubejs/server_scripts/tags.js
@@ -0,0 +1,5 @@
+ServerEvents.tags('item', event => {
+    event.add('iceandfire:dragon_food_meat', 'alexsdelight:raw_bison')
+    event.add('iceandfire:dragon_food_meat', 'alexsdelight:cooked_bison')
+    event.add('iceandfire:dragon_food_meat', 'alexsdelight:bison_mince')
+})
