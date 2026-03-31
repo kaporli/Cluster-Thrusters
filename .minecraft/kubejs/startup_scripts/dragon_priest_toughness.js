52a00ca358a590bd691d8589569adea112bfa3b9
Elias <csoreghelias@gmail.com>
2026-03-31 13:49:42 +0200

    dragon priest rebalance

---
+++ .minecraft/kubejs/startup_scripts/dragon_priest_toughness.js
@@ -0,0 +1,71 @@
+// priority: 0
+
+const AttributeModifier = Java.loadClass('net.minecraft.world.entity.ai.attributes.AttributeModifier')
+const Attributes = Java.loadClass('net.minecraft.world.entity.ai.attributes.Attributes')
+const EquipmentSlot = Java.loadClass('net.minecraft.world.entity.EquipmentSlot')
+const DragonArmorMaterials = Java.loadClass('net.acetheeldritchking.ice_and_fire_spellbooks.items.armor.DragonArmorMaterials')
+const HashMultimap = Java.loadClass('com.google.common.collect.HashMultimap')
+
+const PRIEST_MATERIALS = [
+  DragonArmorMaterials.FIRE_DRAGON_PRIEST,
+  DragonArmorMaterials.ICE_DRAGON_PRIEST,
+  DragonArmorMaterials.LIGHTNING_DRAGON_PRIEST
+]
+
+const ARMOR_SLOTS = [
+  EquipmentSlot.HEAD,
+  EquipmentSlot.CHEST,
+  EquipmentSlot.LEGS,
+  EquipmentSlot.FEET
+]
+
+let patched = false
+
+ItemEvents.modification(() => {
+  if (patched) {
+    return
+  }
+
+  patched = true
+
+  try {
+  PRIEST_MATERIALS.forEach(material => {
+    const slotMap = material.getSlotToAttributeMap()
+
+    ARMOR_SLOTS.forEach(slot => {
+      const attributes = slotMap.get(slot)
+      if (!attributes) {
+        return
+      }
+
+      const mutableAttributes = HashMultimap.create(attributes)
+
+      const toughnessMods = mutableAttributes.get(Attributes.ARMOR_TOUGHNESS)
+      if (!toughnessMods || toughnessMods.size() === 0) {
+        return
+      }
+
+      const copy = []
+      const iterator = toughnessMods.iterator()
+      while (iterator.hasNext()) {
+        copy.push(iterator.next())
+      }
+
+      mutableAttributes.removeAll(Attributes.ARMOR_TOUGHNESS)
+
+      copy.forEach(mod => {
+        mutableAttributes.put(
+          Attributes.ARMOR_TOUGHNESS,
+          new AttributeModifier(mod.getId(), mod.getName(), 2.0, mod.getOperation())
+        )
+      })
+
+      slotMap.put(slot, mutableAttributes)
+    })
+  })
+
+    console.log('[Dragon Priest Toughness] Patched dragon priest armor material toughness to 2.0')
+  } catch (e) {
+    console.log('[Dragon Priest Toughness] Patch failed: ' + e)
+  }
+})
