46a784be1d03ec90917f4467dee8b65aa49d3066
elias <csoreghelias@gmail.com>
2026-04-01 11:47:37 +0200

    remove iaf spellbooks

--- .minecraft/kubejs/startup_scripts/dragon_priest_toughness.js
+++ .minecraft/kubejs/startup_scripts/dragon_priest_toughness.js
@@ -1,71 +1,77 @@
 // priority: 0
 
-const AttributeModifier = Java.loadClass('net.minecraft.world.entity.ai.attributes.AttributeModifier')
-const Attributes = Java.loadClass('net.minecraft.world.entity.ai.attributes.Attributes')
-const EquipmentSlot = Java.loadClass('net.minecraft.world.entity.EquipmentSlot')
-const DragonArmorMaterials = Java.loadClass('net.acetheeldritchking.ice_and_fire_spellbooks.items.armor.DragonArmorMaterials')
-const HashMultimap = Java.loadClass('com.google.common.collect.HashMultimap')
+const MOD_ID = 'ice_and_fire_spellbooks'
 
-const PRIEST_MATERIALS = [
-  DragonArmorMaterials.FIRE_DRAGON_PRIEST,
-  DragonArmorMaterials.ICE_DRAGON_PRIEST,
-  DragonArmorMaterials.LIGHTNING_DRAGON_PRIEST
-]
+if (!Platform.isLoaded(MOD_ID)) {
+  console.log('[Dragon Priest Toughness] Skipping patch: ' + MOD_ID + ' is not loaded')
+} else {
+  const AttributeModifier = Java.loadClass('net.minecraft.world.entity.ai.attributes.AttributeModifier')
+  const Attributes = Java.loadClass('net.minecraft.world.entity.ai.attributes.Attributes')
+  const EquipmentSlot = Java.loadClass('net.minecraft.world.entity.EquipmentSlot')
+  const DragonArmorMaterials = Java.loadClass('net.acetheeldritchking.ice_and_fire_spellbooks.items.armor.DragonArmorMaterials')
+  const HashMultimap = Java.loadClass('com.google.common.collect.HashMultimap')
 
-const ARMOR_SLOTS = [
-  EquipmentSlot.HEAD,
-  EquipmentSlot.CHEST,
-  EquipmentSlot.LEGS,
-  EquipmentSlot.FEET
-]
+  const PRIEST_MATERIALS = [
+    DragonArmorMaterials.FIRE_DRAGON_PRIEST,
+    DragonArmorMaterials.ICE_DRAGON_PRIEST,
+    DragonArmorMaterials.LIGHTNING_DRAGON_PRIEST
+  ]
 
-let patched = false
+  const ARMOR_SLOTS = [
+    EquipmentSlot.HEAD,
+    EquipmentSlot.CHEST,
+    EquipmentSlot.LEGS,
+    EquipmentSlot.FEET
+  ]
 
-ItemEvents.modification(() => {
-  if (patched) {
-    return
-  }
+  let patched = false
 
-  patched = true
+  ItemEvents.modification(() => {
+    if (patched) {
+      return
+    }
 
-  try {
-  PRIEST_MATERIALS.forEach(material => {
-    const slotMap = material.getSlotToAttributeMap()
+    patched = true
 
-    ARMOR_SLOTS.forEach(slot => {
-      const attributes = slotMap.get(slot)
-      if (!attributes) {
-        return
-      }
+    try {
+      PRIEST_MATERIALS.forEach(material => {
+        const slotMap = material.getSlotToAttributeMap()
 
-      const mutableAttributes = HashMultimap.create(attributes)
+        ARMOR_SLOTS.forEach(slot => {
+          const attributes = slotMap.get(slot)
+          if (!attributes) {
+            return
+          }
 
-      const toughnessMods = mutableAttributes.get(Attributes.ARMOR_TOUGHNESS)
-      if (!toughnessMods || toughnessMods.size() === 0) {
-        return
-      }
+          const mutableAttributes = HashMultimap.create(attributes)
 
-      const copy = []
-      const iterator = toughnessMods.iterator()
-      while (iterator.hasNext()) {
-        copy.push(iterator.next())
-      }
+          const toughnessMods = mutableAttributes.get(Attributes.ARMOR_TOUGHNESS)
+          if (!toughnessMods || toughnessMods.size() === 0) {
+            return
+          }
 
-      mutableAttributes.removeAll(Attributes.ARMOR_TOUGHNESS)
+          const copy = []
+          const iterator = toughnessMods.iterator()
+          while (iterator.hasNext()) {
+            copy.push(iterator.next())
+          }
 
-      copy.forEach(mod => {
-        mutableAttributes.put(
-          Attributes.ARMOR_TOUGHNESS,
-          new AttributeModifier(mod.getId(), mod.getName(), 2.0, mod.getOperation())
-        )
+          mutableAttributes.removeAll(Attributes.ARMOR_TOUGHNESS)
+
+          copy.forEach(mod => {
+            mutableAttributes.put(
+              Attributes.ARMOR_TOUGHNESS,
+              new AttributeModifier(mod.getId(), mod.getName(), 2.0, mod.getOperation())
+            )
+          })
+
+          slotMap.put(slot, mutableAttributes)
+        })
       })
 
-      slotMap.put(slot, mutableAttributes)
-    })
+      console.log('[Dragon Priest Toughness] Patched dragon priest armor material toughness to 2.0')
+    } catch (e) {
+      console.log('[Dragon Priest Toughness] Patch failed: ' + e)
+    }
   })
-
-    console.log('[Dragon Priest Toughness] Patched dragon priest armor material toughness to 2.0')
-  } catch (e) {
-    console.log('[Dragon Priest Toughness] Patch failed: ' + e)
-  }
-})
+}
